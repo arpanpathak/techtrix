eb0a106a0f436eac245dbe8d1f9e0ed2e1c4abb4
arpanpathak <arpanpathak1996@gmail.com>
2016-11-15 18:34:02 +0530

    First

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <mega name="viewport" content="width=device-width,initial-scale=1.0">
+  <title> TECHTRIX </title>
+  
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/fontawesome/css/font-awesome.min.css">  
+      <script type="text/javascript" src="script/jquery.js"></script>
+      <script type="text/javascript" src="script/script.js"></script>
+</head>
+
+<body>
+  <script src="js/source.js" >
+</script>
+  <script src="js/index.js"></script>
+<header>
+  <span id="other_events"><i class="fa fa-bars" aria-hidden="true"></i>
+  </span>
+  <h2> <img src="logo.png" style="height: 68px; width: 68px; margin-bottom: -20px;" />
+      TECHTRIX Quiz Platform
+  <span id="signup"><i class="fa fa-user-plus" aria-hidden="true"></i>
+  </span></h2>
+</header>
+
+<div id="d0">
+  <div class="items">
+    <h1><i class="fa fa-code" aria-hidden="true"></i> Coding</h1><hr />
+  </div>
+  
+  <div class="items">
+    <h1><i class="fa fa-bug" aria-hidden="true"></i> BugsFunny</h1><hr />
+    <p style="color:  #3b3c3c;">Bugs are not funny at all! They are frustrating.Debug them if you can</p>
+  </div>
+
+
+  <div class="items">
+    <h1><i class="fa fa-bitbucket-square" aria-hidden="true"></i> BizTech</h1><hr />
+  </div>
+</div>
+<div id="left">
+</div>    
+
+</body>
+</html>
---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('#other_events').on('click',function(){
+		$('#left').slideToggle('fast');
+	});
+	$('#other_events').on('blur',function(){
+		$('#left').slideToggle('fast');
+	});
+});
