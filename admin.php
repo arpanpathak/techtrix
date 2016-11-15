7eab9fb9c93f57472cf8e26b0424604d53b8422d
arpanpathak <arpanpathak1996@gmail.com>
2016-11-16 00:18:55 +0530

    Added

--- admin.php
+++ admin.php
@@ -19,27 +19,30 @@
   <span id="other_events"><i class="fa fa-bars" aria-hidden="true"></i>
   </span>
   <h2> <img src="logo.png" style="height: 68px; width: 68px; margin-bottom: -20px;" />
-      TECHTRIX Quiz Platform
+      TECHTRIX Quiz Platform Admin
   <span id="signup"><i class="fa fa-user-plus" aria-hidden="true"></i>
   </span></h2>
 </header>
 
 <div id="d0">
   <div class="items">
-    <h1><i class="fa fa-code" aria-hidden="true"></i> Coding</h1><hr />
-    <button class="btn-green float-bottom" type="button">EXPLORE</button>
+    <h1><i class="fa fa-code" aria-hidden="true"></i> Create Contest</h1><hr />
+    <button class="btn-green float-bottom" type="button"><i class="fa fa-plus" aria-hidden="true"></i>
+      Create</button>
   </div>
   
   <div class="items">
-    <h1><i class="fa fa-bug" aria-hidden="true"></i> BugsFunny</h1><hr />
-    <p style="color:  #3b3c3c;">Bugs are not funny at all! They are frustrating.Debug them if you can</p>
-    <button class="btn-green float-bottom" type="button">EXPLORE</button>
+    <h1><i class="fa fa-bug" aria-hidden="true"></i> Edit Contest</h1><hr />
+    <p style="color:  #3b3c3c;">Edit Contest Streucture.</p>
+    <button class="btn-green float-bottom" type="button"><i class="fa fa-pencil" aria-hidden="true"></i>
+ EDIT</button>
   </div>
 
 
   <div class="items">
-    <h1><i class="fa fa-bitbucket-square" aria-hidden="true"></i> BizTech</h1><hr />
-    <button class="btn-green float-bottom" type="button">EXPLORE</button>
+    <h1><i class="fa fa-bitbucket-square" aria-hidden="true"></i> Open Contest</h1><hr />
+    <button class="btn-green float-bottom" type="button"><i class="fa fa-play" aria-hidden="true"></i>
+ CLICK</button>
   </div>
 </div>
 <div id="left">
