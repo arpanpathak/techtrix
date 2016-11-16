c77d331565104edf0e2d2579f33ef5a72443bef9
Gunjan4542 <gdbhowmick@gmail.com>
2016-11-16 17:41:49 +0530

    Quiz.php

---
+++ quiz.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BUG FUNNY</title>
+	<link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/fontawesome/css/font-awesome.min.css">  
+      <script type="text/javascript" src="script/jquery.js"></script>
+      <script type="text/javascript" src="script/script.js"></script>
+</head>
+<body>
+	<div id="sidebar">
+		<div class="flat-button">1</div>
+	</div>
+</body>
+</html>
