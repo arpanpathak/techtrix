7a710f7d9c5884913e611892e7e5701e6e152315
arpanpathak <arpanpathak1996@gmail.com>
2016-11-26 19:47:12 +0530

    Spelling Mistake COrrected =P

--- classes/bugsfunny.php
+++ classes/bugsfunny.php
@@ -36,9 +36,8 @@ class BugsFunny extends Contest
 	}
 }
 // Testing ...
-$obj=new BugsFunny(new Team("1234","psudo_coders","Albert","Dennis","ddasd","zzhj"));
+$obj=new BugsFunny(new Team("1234","pseudo_coders","Albert","Dennis","ddasd","zzhj"));
 $arr=$obj->getInfo();
-
 
 foreach ($arr as $key => $value) {
     echo "[ $key ] ----> $value<br />";
