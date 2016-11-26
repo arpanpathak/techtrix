18c0abaeda1efe7f8e4192f5792ea7a3fc497275
arpanpathak <arpanpathak1996@gmail.com>
2016-11-26 11:53:27 +0530

    Timmer Added

--- quiz.php
+++ quiz.php
@@ -9,10 +9,16 @@
       <link rel="stylesheet" href="css/fontawesome/css/font-awesome.min.css">  
       <script type="text/javascript" src="script/jquery.js"></script>
       <script type="text/javascript" src="script/script.js"></script>
+
+      <link rel="stylesheet" href="timer/compiled/flipclock.css">
+	  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	  <script src="timer/compiled/flipclock.js"></script>
+
 </head>
 <body>
 	<header style="margin:0px 0px 2px 0px">
 	<h1 ><i class="fa fa-bug"   aria-hidden="true"></i> BugsFunny</h1>
+
 	</header>
 	<div id="sidebar">
 		<div class="flat-button">1</div>
@@ -32,6 +38,7 @@
 		<div class="flat-button">14</div>
 	</div>
 	<div id="q1" class="question-view" >
+	<span class="ques-no">1)</span>
 	<h3 style="font-family:sans-serif">What are the reason the this question will not run?</h3>
 	<table style=" margin:auto;margin-top:5%;width:inherit">
 		<tr>
@@ -42,7 +49,19 @@
 			<td class="option"><label><input  type="radio" name="option1">Option 1</label></td>
 			<td class="option"><label ><input  type="radio" name="option1">Option 1</label></td>
 		</tr>
+		<tr>
+		<td><button type="button" class="flat-btn-move prev">
+			<i class="fa fa-arrow-left" aria-hidden="true"></i> Previous</button>
+		</td>
+		<td><button type="button" class="flat-btn-move next">
+			Next <i class="fa fa-arrow-right" aria-hidden="true"></i> </button>
+		</td>
+		</tr>
 	</table>
+	</div>
+	<div class="timer_container">
+		<div class="clock" style="margin:2em;"></div>
+		<div class="message"></div>
 	</div>
 </body>
 </html>
---
+++ structure.php
@@ -0,0 +1,12 @@
+<?php
+/*** This PHP file contains all the necessary attributes 
+	@author: Arpan Pathak ***/
+class Contest{
+	private $contest_name="";
+	private $duration; // duration in seconds...
+	private $contest_db_name="techtrix"; // default database name is techtrix
+	private $prize_money;
+	private $ques_count=10; // default value is 10; 
+}
+
+?>
