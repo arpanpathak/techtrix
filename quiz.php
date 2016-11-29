06f7503f0f1e240392470c63d297478112f2689e
arpanpathak <arpanpathak1996@gmail.com>
2016-11-29 13:17:49 +0530

    Code.cpp added

--- quiz.php
+++ quiz.php
@@ -22,6 +22,65 @@
       <link rel="stylesheet" href="timer/compiled/flipclock.css">
 	  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	  <script src="timer/compiled/flipclock.js"></script>
+	  
+	  <!-- icnluding codemirror files !-->
+	   	<script src="codemirror/lib/codemirror.js"></script>
+		<link rel="stylesheet" href="codemirror/lib/codemirror.css">
+		<script src="codemirror/mode/javascript/javascript.js"></script>
+		<link rel="stylesheet" type="text/css" href="codemirror/theme/blackboard.css">
+		<link rel="stylesheet" href="codemirror/theme/3024-day.css">
+	<link rel="stylesheet" href="codemirror/theme/3024-night.css">
+	<link rel="stylesheet" href="codemirror/theme/abcdef.css">
+	<link rel="stylesheet" href="codemirror/theme/ambiance.css">
+	<link rel="stylesheet" href="codemirror/theme/base16-dark.css">
+	<link rel="stylesheet" href="codemirror/theme/bespin.css">
+	<link rel="stylesheet" href="codemirror/theme/base16-light.css">
+	<link rel="stylesheet" href="codemirror/theme/blackboard.css">
+	<link rel="stylesheet" href="codemirror/theme/cobalt.css">
+	<link rel="stylesheet" href="codemirror/theme/colorforth.css">
+	<link rel="stylesheet" href="codemirror/theme/dracula.css">
+	<link rel="stylesheet" href="codemirror/theme/eclipse.css">
+	<link rel="stylesheet" href="codemirror/theme/elegant.css">
+	<link rel="stylesheet" href="codemirror/theme/erlang-dark.css">
+	<link rel="stylesheet" href="codemirror/theme/hopscotch.css">
+	<link rel="stylesheet" href="codemirror/theme/icecoder.css">
+	<link rel="stylesheet" href="codemirror/theme/isotope.css">
+	<link rel="stylesheet" href="codemirror/theme/lesser-dark.css">
+	<link rel="stylesheet" href="codemirror/theme/liquibyte.css">
+	<link rel="stylesheet" href="codemirror/theme/material.css">
+	<link rel="stylesheet" href="codemirror/theme/mbo.css">
+	<link rel="stylesheet" href="codemirror/theme/mdn-like.css">
+	<link rel="stylesheet" href="codemirror/theme/midnight.css">
+	<link rel="stylesheet" href="codemirror/theme/monokai.css">
+	<link rel="stylesheet" href="codemirror/theme/neat.css">
+	<link rel="stylesheet" href="codemirror/theme/neo.css">
+	<link rel="stylesheet" href="codemirror/theme/night.css">
+	<link rel="stylesheet" href="codemirror/theme/panda-syntax.css">
+	<link rel="stylesheet" href="codemirror/theme/paraiso-dark.css">
+	<link rel="stylesheet" href="codemirror/theme/paraiso-light.css">
+	<link rel="stylesheet" href="codemirror/theme/pastel-on-dark.css">
+	<link rel="stylesheet" href="codemirror/theme/railscasts.css">
+	<link rel="stylesheet" href="codemirror/theme/rubyblue.css">
+	<link rel="stylesheet" href="codemirror/theme/seti.css">
+	<link rel="stylesheet" href="codemirror/theme/solarized.css">
+	<link rel="stylesheet" href="codemirror/theme/the-matrix.css">
+	<link rel="stylesheet" href="codemirror/theme/tomorrow-night-bright.css">
+	<link rel="stylesheet" href="codemirror/theme/tomorrow-night-eighties.css">
+	<link rel="stylesheet" href="codemirror/theme/ttcn.css">
+	<link rel="stylesheet" href="codemirror/theme/twilight.css">
+	<link rel="stylesheet" href="codemirror/theme/vibrant-ink.css">
+	<link rel="stylesheet" href="codemirror/theme/xq-dark.css">
+	<link rel="stylesheet" href="codemirror/theme/xq-light.css">
+	<link rel="stylesheet" href="codemirror/theme/yeti.css">
+	<link rel="stylesheet" href="codemirror/theme/zenburn.css">
+	<script src="codemirror/addon/edit/closebrackets.js"></script>
+	<!-- codemioor adons files -->
+	<script src="codemirror/addon/edit/matchbrackets.js"></script>
+	<link rel="stylesheet" href="codemirror/addon/hint/show-hint.css">
+	<script src="codemirror/addon/hint/show-hint.js"></script>
+	<script src="codemirror/mode/clike/clike.js" type="text/javascript"></script>
+	<!--end of codemirror files -->
+
 
 </head>
 <body>
@@ -42,7 +101,7 @@
 			<div id=q$i class=question-view style='".$disp."'>
 			<span class=ques-no>$i)</span>
 			<h3 style='font-family:sans-serif'>What are the reason the this question will not run?</h3>
-			<textarea  class='code' readonly>".readIt('script/script.js')."</textarea>
+			<textarea  class='code' readonly>".readIt('code.cpp')."</textarea>
 			<table style=' margin:auto;margin-top:5%;width:inherit'>
 				<tr>
 					<td class=option ><label ><input  type=radio name=q$i class=opt1>Option 1</label></td>
